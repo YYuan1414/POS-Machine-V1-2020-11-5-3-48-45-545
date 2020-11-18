697db72edc677978e3158758ea693c88f8e22442
YYuan1414 <yuanyiren19900728@163.com>
2020-11-18 22:26:47 +0800

    feat: add items with sale

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -12,7 +12,7 @@ export function printReceipt(tags: string[]): string {
   function getItemsInformation(tags: string[]): Item[] {
     tag = getEachItemQuantity(tags)
     items = getEachItemInformation(tag)
-    items = getEachItemSaleInformation(tags)
+    items = getEachItemSaleInformation(items)
     return items
   }
 
@@ -30,7 +30,7 @@ export function printReceipt(tags: string[]): string {
       {
         itemQuantity=1
         tagIndex++
-        index++
+        index = row
       }
       tag[tagIndex]={
         barcode: tags[row],
@@ -46,6 +46,7 @@ export function printReceipt(tags: string[]): string {
     for (let itemsNumber=0;itemsNumber<tag.length;itemsNumber++){
       const item = allItems.filter(item => item.barcode === tag[itemsNumber].barcode)
       items[itemsNumber] = {
+        barcode: item[0].barcode,
         name: item[0].name,
         price: item[0].price,
         unit: item[0].unit,
@@ -58,15 +59,26 @@ export function printReceipt(tags: string[]): string {
 
 
 
-  function getEachItemSaleInformation(tags: string[]): Item[] {
+  function getEachItemSaleInformation(items: Item[]): Item[] {
+    const saleItems=loadPromotions()[0].barcodes
+    const row=0
+    for (let saleItemsNumber=0;saleItemsNumber<saleItems.length;saleItemsNumber++){
+
+      for(let itemsNumber=0;itemsNumber<items.length;itemsNumber++){
+        if (items[itemsNumber].barcode===saleItems[saleItemsNumber])
+        {
+          items[itemsNumber].isSaleItem=true
+        }
+
+      }
+    }
     return items
   }
 
   function generateReceipt(items: Item[]): string {
-    receipt = "***<store earning no money>Receipt ***\n"
+    receipt = '***<store earning no money>Receipt ***\n'
       + generateItemsReceiptWithoutDiscountprice(items)
-      + `Discounted prices：0(yuan)\n`
-      + `**********************`
+      + generateSalePriceReceipt(items)
     return receipt
   }
 
@@ -78,8 +90,22 @@ export function printReceipt(tags: string[]): string {
       partOfReceipt+=`Name：${items[itemsNumber].name}，Quantity：${items[itemsNumber].quantity} ${items[itemsNumber].unit}s，Unit：${items[itemsNumber].price.toFixed(2)}(yuan)，Subtotal：${(items[itemsNumber].price * items[itemsNumber].quantity).toFixed(2)}(yuan)\n`
       totalPrice +=(items[itemsNumber].price * items[itemsNumber].quantity)
     }
-    partOfReceipt+=`----------------------\n`+`Total：${(totalPrice).toFixed(2)}(yuan)\n`
+    partOfReceipt+='----------------------\n'+`Total：${(totalPrice).toFixed(2)}(yuan)\n`
     return partOfReceipt
+  }
+
+  function generateSalePriceReceipt(items: Item[]): string {
+    let salePriceReceipt = ''
+    let discountedPrice=0
+    for (let itemsNumber=0; itemsNumber<items.length;itemsNumber++)
+    {
+      if (items[itemsNumber].isSaleItem===true)
+      {
+        discountedPrice+=Math.floor(items[itemsNumber].quantity/3)*items[itemsNumber].price
+      }
+    }
+    salePriceReceipt=`Discounted prices：${discountedPrice.toFixed(2)}(yuan)\n**********************`
+    return salePriceReceipt
   }
 
 
@@ -97,6 +123,7 @@ Discounted prices：7.50(yuan)
 
 
 interface Item {
+  barcode:string,
   name: string,
   unit: string,
   price: number,
--- test/PrintReceipt.test.ts
+++ test/PrintReceipt.test.ts
@@ -1,57 +1,81 @@
 import { printReceipt } from '../src/PrintReceipt'
 
 describe('printReceipt', () => {
-  /* it('should print receipt with promotion when print receipt', () => {
-    const tags = [
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000003-2.5',
-      'ITEM000005',
-      'ITEM000005-2',
-    ]
-
-    const expectText = `***<store earning no money>Receipt ***
-Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
-Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
-----------------------
-Total：58.50(yuan)
-Discounted prices：7.50(yuan)
-**********************` */
+  /*   it('should print receipt with promotion when print receipt', () => {
+      const tags = [
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000001',
+        'ITEM000003-2.5',
+        'ITEM000005',
+        'ITEM000005-2',
+      ]
+      const expectText = `***<store earning no money>Receipt ***
+  Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
+  Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
+  Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
+  ----------------------
+  Total：58.50(yuan)
+  Discounted prices：7.50(yuan)
+  **********************`
+  expect(printReceipt(tags)).toEqual(expectText)
+    }) */
 
   it('should print receipt with promotion when print receipt', () => {
     const tags = [
-      'ITEM000001'
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003',
+      'ITEM000005',
+      'ITEM000005',
     ]
 
     const expectText = `***<store earning no money>Receipt ***
-Name：Sprite，Quantity：1 bottles，Unit：3.00(yuan)，Subtotal：3.00(yuan)
+Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：15.00(yuan)
+Name：Litchi，Quantity：1 pounds，Unit：15.00(yuan)，Subtotal：15.00(yuan)
+Name：Instant Noodles，Quantity：2 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
 ----------------------
-Total：3.00(yuan)
-Discounted prices：0(yuan)
+Total：39.00(yuan)
+Discounted prices：3.00(yuan)
+**********************`
+    expect(printReceipt(tags)).toEqual(expectText)
+  })
+
+  it('should print receipt without promotion when print receipt of one item', () => {
+    const tags = [
+      'ITEM000002'
+    ]
+
+    const expectText = `***<store earning no money>Receipt ***
+Name：Apple，Quantity：1 pounds，Unit：5.50(yuan)，Subtotal：5.50(yuan)
+----------------------
+Total：5.50(yuan)
+Discounted prices：0.00(yuan)
 **********************`
 
     expect(printReceipt(tags)).toEqual(expectText)
   })
 
-  it('should print receipt with promotion when print receipt for numbers of item', () => {
+  it('should print receipt without promotion when print receipt for numbers of item', () => {
     const tags = [
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000005',
+      'ITEM000002',
+      'ITEM000002',
+      'ITEM000002',
+      'ITEM000002',
+      'ITEM000003',
     ]
 
     const expectText = `***<store earning no money>Receipt ***
-Name：Sprite，Quantity：4 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-Name：Instant Noodles，Quantity：1 bags，Unit：4.50(yuan)，Subtotal：4.50(yuan)
+Name：Apple，Quantity：4 pounds，Unit：5.50(yuan)，Subtotal：22.00(yuan)
+Name：Litchi，Quantity：1 pounds，Unit：15.00(yuan)，Subtotal：15.00(yuan)
 ----------------------
-Total：16.50(yuan)
-Discounted prices：0(yuan)
+Total：37.00(yuan)
+Discounted prices：0.00(yuan)
 **********************`
 
     expect(printReceipt(tags)).toEqual(expectText)
